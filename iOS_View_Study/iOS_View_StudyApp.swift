dc099f026c70cf344ec4af0a32ee52aa600e6fa0
rudqlsdla <hsmikvyv@gmail.com>
2025-01-21 21:42:16 +0900

    feat($project): 프로젝트 생성

---
+++ iOS_View_Study/iOS_View_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_View_StudyApp.swift
+//  iOS_View_Study
+//
+//  Created by 임경빈 on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct iOS_View_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

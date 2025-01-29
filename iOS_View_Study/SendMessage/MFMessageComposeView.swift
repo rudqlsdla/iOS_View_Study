2d6211cda3ab1c15caa80d9ed5fe7aef80d3c370
rudqlsdla <hsmikvyv@gmail.com>
2025-01-29 15:57:58 +0900

    feat($message): 메시지 전송이 끝나면 신호를 받을 수 있는 coordinator 구현

--- iOS_View_Study/SendMessage/MFMessageComposeView.swift
+++ iOS_View_Study/SendMessage/MFMessageComposeView.swift
@@ -15,11 +15,38 @@ struct MFMessageComposeView: UIViewControllerRepresentable {
     let viewController = MFMessageComposeViewController()
     viewController.recipients = ["메시지를 받을 사람"]
     viewController.body = "메시지를 보낼 내용이 담기는 body"
+    viewController.messageComposeDelegate = context.coordinator
     return viewController
   }
   
   func updateUIViewController(_ uiViewController: MFMessageComposeViewController, context: Context) {
+  }
+  
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(self)
+  }
+}
 
+class Coordinator: NSObject, MFMessageComposeViewControllerDelegate {
+  var parent: MFMessageComposeView
+  
+  init(_ parent: MFMessageComposeView) {
+    self.parent = parent
+  }
+  
+  func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+    print(result)
+    switch result {
+    case .cancelled:
+      print("메시지 전송 취소")
+    case .sent:
+      print("메시지 전송 성공")
+    case .failed:
+      print("메시지 전송 실패")
+    @unknown default:
+      break
+    }
+    controller.dismiss(animated: true, completion: nil)
   }
 }
 

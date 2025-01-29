b2cf1f9298442e00d690e31981abde657df2fd71
rudqlsdla <hsmikvyv@gmail.com>
2025-01-29 16:02:18 +0900

    feat($message): 메시지 전송 화면을 보여주는 동작 및 버튼 추가

--- iOS_View_Study/SendMessage/SendMessageView.swift
+++ iOS_View_Study/SendMessage/SendMessageView.swift
@@ -5,11 +5,29 @@
 //  Created by 임경빈 on 1/28/25.
 //
 
+import MessageUI
 import SwiftUI
 
 struct SendMessageView: View {
+  @State private var isMessageViewPresented = false
+  
   var body: some View {
-    Text("메시지 보내기")
+    Button {
+      showMessageView()
+    } label: {
+      Text("메시지 전송 버튼")
+    }
+    .sheet(isPresented: $isMessageViewPresented) {
+      MFMessageComposeView()
+    }
+  }
+}
+
+private extension SendMessageView {
+  func showMessageView() {
+    if MFMessageComposeViewController.canSendText() { // 메시지를 보낼 수 있는지 확인
+      isMessageViewPresented = true
+    }
   }
 }
 

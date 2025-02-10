2aab248f8f04c16dbc1d0fc9b2f4049e4e0deae6
rudqlsdla <hsmikvyv@gmail.com>
2025-02-10 21:30:05 +0900

    feat($motion): ShakeMotion을 활용하여 기기를 흔들었을 때, QR 코드가 나타나는 기능 구현

--- iOS_View_Study/MotionShake/MotionShakeView.swift
+++ iOS_View_Study/MotionShake/MotionShakeView.swift
@@ -8,8 +8,30 @@
 import SwiftUI
 
 struct MotionShakeView: View {
+  @State private var isQRViewPresented = false
+  
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    Text("흔들어주세요!")
+      .onReceive(NotificationCenter.default.publisher(for: .deviceDidShakeNotification)) { _ in
+        isQRViewPresented = true
+      }
+      .sheet(isPresented: $isQRViewPresented) {
+        Image("ShakeQR")
+          .resizable()
+          .scaledToFit()
+          .padding()
+      }
+  }
+}
+
+extension NSNotification.Name {
+  public static let deviceDidShakeNotification = NSNotification.Name("DeviceDidShakeNotification")
+}
+
+extension UIWindow {
+  open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+    super.motionEnded(motion, with: event)
+    NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
   }
 }
 

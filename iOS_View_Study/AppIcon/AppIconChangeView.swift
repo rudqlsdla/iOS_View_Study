a83a0e4a617f6f63c01f103cec78060d8f4450d1
rudqlsdla <hsmikvyv@gmail.com>
2025-02-25 22:59:58 +0900

    feat($icon): 앱 아이콘을 동적으로 변경하는 로직 추가

--- iOS_View_Study/AppIcon/AppIconChangeView.swift
+++ iOS_View_Study/AppIcon/AppIconChangeView.swift
@@ -10,7 +10,11 @@ import SwiftUI
 struct AppIconChangeView: View {
   var body: some View {
     Button {
-
+      UIApplication.shared.setAlternateIconName("secondaryIcon") { error in
+        if let error = error {
+          print(error.localizedDescription)
+        }
+      }
     } label: {
       Text("앱 아이콘 변경하기")
     }

2c16745b53a13efa188392236eb294455e892476
rudqlsdla <hsmikvyv@gmail.com>
2025-03-30 23:46:21 +0900

    chore($endian): 로그 추가

--- Personal/Endian/EndiannessChecker.m
+++ Personal/Endian/EndiannessChecker.m
@@ -16,8 +16,10 @@
   NSLog(@"Stored bytes: %02X %02X %02X %02X", ptr[0], ptr[1], ptr[2], ptr[3]);
   
   if (ptr[0] == 0x78) {
+    NSLog(@"Little Endian");
     return @"LITTLE ENDIAN";  // 78 56 34 12 (LSB 먼저)
   } else if (ptr[0] == 0x12) {
+    NSLog(@"Big Endian");
     return @"BIG ENDIAN";  // 12 34 56 78 (MSB 먼저)
   } else {
     return @"UNKNOWN ENDIAN";  // 예상치 못한 결과
